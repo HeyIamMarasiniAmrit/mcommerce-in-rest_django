b9cc2b563642be4e4d328039064e01be32102f12
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-04-11 13:12:01 +0545

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import polls
+from .models import polls,ProductCategory
 
 # Register your models here.
 admin.site.register(polls)
+admin.site.register(ProductCategory)

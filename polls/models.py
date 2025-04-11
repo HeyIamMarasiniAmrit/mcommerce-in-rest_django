69a76be2f8408552fcc7088055c1151b5a409ea0
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-04-11 13:11:42 +0545

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Product Category model
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=100)
+    category_id = models.PositiveIntegerField()
+
+
 class polls(models.Model):
+    category_name = models.ForeignKey('ProductCategory', related_name='productCategory', on_delete=models.CASCADE,null=True)
     pr_id = models.PositiveIntegerField()
-    name=models.CharField(max_length=50)
-    cost=models.DecimalField(decimal_places=2, max_digits=6)
+    name = models.CharField(max_length=50)
+    cost = models.DecimalField(decimal_places=2, max_digits=6)
     date = models.DateTimeField()
     desc = models.TextField()
 
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name

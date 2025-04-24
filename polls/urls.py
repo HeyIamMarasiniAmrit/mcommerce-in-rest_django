03c9b8748a74dc8da422a3f6acb0d3cd71dab8e1
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-04-24 13:07:50 +0545

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,15 @@
 
 from django.urls import path, include
 from . import views
+from polls.views import listpolls
 
 urlpatterns = [
 
-    path('pollslist/',views.listpolls,name='listpolls'),
+    path('pollslist/',views.listPolls,name='listPolls'),
     path('messagelist/',views.listmessages,name='message'),
+    path('classpollslist/', listproducts.as_view(), name='listproducts'),
+    path('classDetailed/', productDetailedView.as_view, name='detailedProduct'
+    path('mixinpath/',views.listproductsMixins.as_view(),name='mp')
+    path('productmixin/<int:pk>',views.DetailedProductMixins.as_view(),name='mdp')
 ]
+

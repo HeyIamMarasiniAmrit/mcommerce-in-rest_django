dfa1f4d74dbdfc4757abcd6f28502e1e1b90e504
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-04-25 12:47:46 +0545

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,13 @@
-
 from django.urls import path, include
 from . import views
 from polls.views import listpolls
+from .views import productDetailsView, listproducts, productviewset
+from rest_framework.routers import defaultRouter
+
+router = defaultRouter()
+router.register(
+    'productviewset',Productviewset,basename='product'
+)
 
 urlpatterns = [
 
@@ -11,5 +17,8 @@ urlpatterns = [
     path('classDetailed/', productDetailedView.as_view, name='detailedProduct'
     path('mixinpath/',views.listproductsMixins.as_view(),name='mp')
     path('productmixin/<int:pk>',views.DetailedProductMixins.as_view(),name='mdp')
-]
+    path('productgenericlist/',views.listproductGenerics.as_view(),name='lpg')
+    path('productgenericdetailed/<int:pk>',views.DetailedproductGenerics.as_view(),name='dpg')
+    path('special/<int:pk>', views.specialproductGenerics.as_view(), name='spg')
 
+]+router.urls
